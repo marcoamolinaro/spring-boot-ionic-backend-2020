1f6b08b32d274e8e1057f211353b64a9060d964b
Marco Molinaro <molinaromarcoaurelio@gmail.com>
2020-02-15 16:33:11 -0300

    First Commit

---
+++ src/main/java/com/scmitltda/cursomc2020/Cursomc2020Application.java
@@ -0,0 +1,13 @@
+package com.scmitltda.cursomc2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomc2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomc2020Application.class, args);
+	}
+
+}
