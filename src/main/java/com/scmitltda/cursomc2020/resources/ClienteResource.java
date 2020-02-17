25ca3b2bf332656c90c843715e0da8dd0424ac50
Marco Molinaro <molinaromarcoaurelio@gmail.com>
2020-02-17 08:03:18 -0300

    22. Endpoint /clientes/{id} disponível

---
+++ src/main/java/com/scmitltda/cursomc2020/resources/ClienteResource.java
@@ -0,0 +1,27 @@
+package com.scmitltda.cursomc2020.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.scmitltda.cursomc2020.domain.Cliente;
+import com.scmitltda.cursomc2020.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	@RequestMapping(value = "{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		
+		Cliente cliente = clienteService.buscar(id);
+		
+		return ResponseEntity.ok().body(cliente);
+	}
+}
